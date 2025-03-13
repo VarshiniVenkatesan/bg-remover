263f0f5410e4a3e121fe42a0e54fa789483c32b9
S.V. VARSHINI <151605516+VarshiniVenkatesan@users.noreply.github.com>
2025-03-13 09:21:00 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate virtual environment (if using one)
+# source venv/bin/activate  
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the bot
+python bot.py
